f229d3e1c81093abb9eee2ebd845b179aba11306
Seowon Choi <chltjdnjs529@gmail.com>
2023-01-06 17:15:12 +0900

    Toonmoa

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:toonmoa/screens/home_screen.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        backgroundColor: const Color(0xffe7626c),
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            color: Color(0xff232b55),
-          ),
-        ),
-        cardColor: const Color(0xfff4eddb),
-      ),
-      home: const Scaffold(
+      home: Scaffold(
         body: HomeScreen(),
       ),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,148 +1,144 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:toonmoa/models/webtoon_model.dart';
+import 'package:toonmoa/services/api_service.dart';
+import 'package:toonmoa/widgets/webtoon_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final day = DateFormat('EEEE').format(DateTime.now()).toUpperCase();
 
-class _HomeScreenState extends State<HomeScreen> {
-  static const twentyFiveMinutes = 1500;
-  int totalSeconds = twentyFiveMinutes;
-  bool isRunning = false;
-  int totalPomodoros = 0;
-  late Timer timer;
-
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      setState(() {
-        totalPomodoros++;
-        isRunning = false;
-        totalSeconds = twentyFiveMinutes;
-      });
-      timer.cancel();
-    } else {
-      setState(() {
-        totalSeconds = totalSeconds - 1;
-      });
-    }
-  }
-
-  void onStartPressed() {
-    timer = Timer.periodic(
-      const Duration(seconds: 1),
-      onTick,
-    );
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  void onPausePressed() {
-    timer.cancel();
-    setState(() {
-      isRunning = false;
-    });
-  }
-
-  void restart() {
-    setState(() {
-      isRunning = false;
-      totalSeconds = twentyFiveMinutes;
-    });
-    timer.cancel();
-  }
-
-  String format(int seconds) {
-    var duration = Duration(seconds: seconds);
-    return duration.toString().split(".").first.substring(2, 7);
+  goToNaverWebtoon() {
+    final url = Uri.parse('https://m.comic.naver.com/index');
+    launchUrl(url);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Column(
-        children: [
-          Flexible(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                format(totalSeconds),
-                style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                  fontSize: 89,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
+      backgroundColor: const Color(0xff3e3e3e),
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            "TOONMOA.",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Fredoka',
+              letterSpacing: 1.5,
             ),
           ),
-          Flexible(
-            flex: 3,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: isRunning ? onPausePressed : onStartPressed,
-                    icon: Icon(isRunning
-                        ? Icons.pause_circle_outline
-                        : Icons.play_circle_outline),
-                    iconSize: 120,
-                    color: Theme.of(context).cardColor,
-                  ),
-                  IconButton(
-                    onPressed: restart,
-                    icon: const Icon(Icons.replay_outlined),
-                    iconSize: 50,
-                    color: Theme.of(context).cardColor,
-                  ),
-                  Text('Restart',
-                      style: TextStyle(
-                          color: Theme.of(context).cardColor, fontSize: 20))
-                ],
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Row(
+        ),
+        foregroundColor: const Color(0xff3e3e3e),
+        backgroundColor: Colors.amber,
+        elevation: 0,
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
               children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).cardColor,
-                        borderRadius: BorderRadius.circular(55)),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Pomodoros',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color: Theme.of(context).textTheme.headline1!.color,
-                          ),
-                        ),
-                        Text(
-                          '$totalPomodoros',
-                          style: TextStyle(
-                            fontSize: 58,
-                            fontWeight: FontWeight.w600,
-                            color: Theme.of(context).textTheme.headline1!.color,
-                          ),
-                        ),
-                      ],
-                    ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  "$day TOONS.",
+                  style: const TextStyle(
+                    fontSize: 30,
+                    color: Color(0xffffc813),
+                    fontFamily: 'Fredoka',
+                    letterSpacing: 2.5,
                   ),
                 ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                ),
+                SizedBox(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 1,
+                        width: 300,
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: goToNaverWebtoon,
+                        child: Container(
+                          width: 250,
+                          margin: const EdgeInsets.symmetric(vertical: 50),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                width: 1,
+                                color: Colors.white.withOpacity(0.3),
+                              ),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(25.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.network(
+                                  'https://seeklogo.com/images/W/webtoon-logo-C86D23EBD9-seeklogo.com.png',
+                                  height: 60,
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                const Text(
+                                  'NAVER 웹툰\n바로가기',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    height: 1.5,
+                                    letterSpacing: 0.5,
+                                    fontFamily: 'Fredoka',
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.amber),
             ),
-          ),
-        ],
+          );
+        },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 70),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Webtoon(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
